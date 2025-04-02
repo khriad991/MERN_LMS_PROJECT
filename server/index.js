ae18311238ef7d2569f20ad041314f16907cf88a
Kamrul Hasan <991khriad@gmail.com>
2025-04-02 22:52:44 +0600

    commit for done APIs of Register & Login for

--- server/index.js
+++ server/index.js
@@ -2,42 +2,32 @@
 import  express from "express";
 import dotenv from "dotenv"
 import connectDB from "./database/db.js";
+import userRoute from "./route/userRoute.js";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
 
 
 dotenv.config({})
 // call database connection
 connectDB()
 
-
-
 const app = express();
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5050",
+    credentials:true
+}))
+
+app.use("/api/v3/user", userRoute)
 
 
-
-
-app.get("/", (req, res) => {
-    res.send(`
-       
-                <style>
-                    h1 {
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        height: 100vh;
-                        margin: 0;
-                        background-color: #f4f4f4;
-                         color: dodgerblue;
-                        font-size: 2.5rem;
-                        text-align: center;
-                    }
-                </style>
-            
-                <h1>🚀 Server is Running Successfully! 🎉</h1>
-      
-    `);
-});
-
-let PORT = process.env.PORT || 8000
+let PORT = process.env.PORT || 5000
 app.listen(PORT , ()=>{
     console.log( `server is running on http://localhost:${PORT}`)
 })
+
+
+
+// end of 2.09
