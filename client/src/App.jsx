13fa506ea78421716cec07c6848513412045f619
Kamrul Hasan <991khriad@gmail.com>
2025-04-02 14:46:38 +0600

    1st comit for setup project client site

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Login from "@/pages/Login.jsx";
+
+function App() {
+
+    return (
+        <main>
+            <Login />
+        </main>
+    );
+}
+
+export default App;
